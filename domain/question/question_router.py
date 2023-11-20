e9859968b1a60c06a2c19716bcd9794fa302fb90
LeeEuiHyoung <kkh60245@gmail.com>
2023-11-20 16:12:02 +0900

    라우터 추가

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -4,6 +4,7 @@ from database import SessionLocal
 from models import Question
 
 # 라우터 파일에 반드시 필요한 것은 APIRouter 클래스로 생성한 router 객체이다. 
+# 라우팅이란 FastAPI가 요청받은 URL을 해석하여 그에 맞는 함수를 실행하여 그 결과를 리턴하는 행위를 말한다.
 # router 객체를 생성하여 FastAPI 앱에 등록해야만 라우팅 기능이 동작한다.
 router = APIRouter(
     prefix="/api/question",
