273ecd314d0ad73bc1d56cc73167c24aae871e0f
LeeEuiHyoung <kkh60245@gmail.com>
2023-12-08 03:01:39 +0900

    MoveNet 추가

--- src/routes/exercise.jsx
+++ src/routes/exercise.jsx
@@ -8,7 +8,8 @@ import {
 } from "@chakra-ui/react";
 
 // tf
-import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tf from "@tensorflow/tfjs";
+import * as poseDetection from "@tensorflow-models/pose-detection";
 
 // Utils
 import { getSpeech } from "../utils/getSpeech";
@@ -21,21 +22,49 @@ import "../App.css";
 
 function Exercise({ props }) {
   const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
   const [value, setValue] = useState("안녕하세요");
+  
+
+  // load posenet
+  const runPosenet = async () => {
+    await tf.ready();
+    const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
+    const net = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+
+    // 계속 감지
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // 비디오 속성 가져오기.
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // 비디오 넓이 지정
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // 디텍션 시작
+      const poses = await net.estimatePoses(video);
+      console.log(poses);
+    }
+  };
+
+  runPosenet();
 
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     console.log(imageSrc);
   };
-
-  // 이미지 출력 결과 받기
-  // const [imgResult, setimgResult] = useState([]);
-
-  // useEffect(() => {
-  //   fastapi("get", "/api/question/list", {}, (json) => {
-  //     setimgResult(json);
-  //   });
-  // }, []);
 
   //음성 변환 목소리 preload
   useEffect(() => {
@@ -54,12 +83,42 @@ function Exercise({ props }) {
   return (
     <SimpleGrid columns={3} spacing={10}>
       <Box>
-        <CircularProgress value={40} color="green" size='400px'>
+        <CircularProgress value={40} color="green" size="400px">
           <CircularProgressLabel>40%</CircularProgressLabel>
         </CircularProgress>
       </Box>
       <Box>
-        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          screenshotFormat="image/jpeg"
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
         <button onClick={capture}>캡쳐</button>
       </Box>
       <Box>
@@ -73,3 +132,12 @@ function Exercise({ props }) {
 }
 
 export default Exercise;
+
+// 이미지 출력 결과 받기
+// const [imgResult, setimgResult] = useState([]);
+
+// useEffect(() => {
+//   fastapi("get", "/api/question/list", {}, (json) => {
+//     setimgResult(json);
+//   });
+// }, []);
